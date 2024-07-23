5b3cb7dd8b1effdceacb1360116fc18365941ea7
jorgeduje <jorgeduje4@gmail.com>
2024-07-22 22:31:11 -0300

    se trabaja con local storage

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-// {
-//     id: 1,
-//     text: "estudiar javascript",
-//     completed: false
+//
+// let todos = JSON.parse(localStorage.getItem("todos")); // null
+// let todos = [];
+
+// let valorInicial = JSON.parse(localStorage.getItem("todos"));
+
+// if (valorInicial !== null) {
+//   // [{}{}{}] !== null --> true
+//   todos = valorInicial;
 // }
 
-let todos = []; // 5
+//  a || b || c || d --> truthy
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
 let contador = 1;
 let addTodoButton = document.getElementById("btn-add"); // btn add
 let todosList = document.getElementById("todo-list"); // container cards
@@ -17,6 +25,7 @@ let addTodo = () => {
   contador++;
 
   pintarTarjetas();
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 addTodoButton.addEventListener("click", addTodo);
@@ -25,11 +34,13 @@ const toogleCompleted = (elemento) => {
   let encontrado = todos.find((todo) => todo.id === elemento.id);
   encontrado.completed = !encontrado.completed; // !false --> true
   pintarTarjetas();
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 const deleteTodo = (elemento) => {
   todos = todos.filter((todo) => todo.id !== elemento.id); // [ ]
   pintarTarjetas();
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 const pintarTarjetas = () => {
@@ -63,3 +74,45 @@ const pintarTarjetas = () => {
 };
 
 pintarTarjetas();
+
+// let botonDeLocalStorage = document.getElementById("local-btn");
+
+// botonDeLocalStorage.addEventListener("click", () => {
+//   localStorage.setItem(
+//     "tareas",
+//     JSON.stringify([
+//       {
+//         id: 1,
+//         text: "estudiar javascript",
+//         completed: false,
+//       },
+//     ])
+//   );
+// });
+
+// // localStorage.setItem(); // ---> guardar info
+// // localStorage.getItem(); // ---> obtener info
+// // localStorage.removeItem(); // ---> eliminar info
+// // localStorage.clear(); // ---> limpiar el localStorage
+
+// let btnGetLocal = document.getElementById("local-btn-get");
+// btnGetLocal.addEventListener("click", () => {
+//   let tareas = JSON.parse(localStorage.getItem("tareas"));
+//   console.log(tareas);
+// });
+
+// let btnRemove = document.getElementById("local-btn-remove");
+// btnRemove.addEventListener("click", () => {
+//   localStorage.removeItem("frase");
+// });
+
+// let btnClear = document.getElementById("local-btn-clear");
+// btnClear.addEventListener("click", () => {
+//   localStorage.clear();
+// });
+
+// let convertir = JSON.parse("true");
+// let convertir = "true";
+// let x = JSON.stringify([{ name: "pepe", id: 2 }]);
+// let convertir = JSON.parse(x);
+// console.log(typeof convertir);
